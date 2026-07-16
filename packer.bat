babf9118ced125a9b2c15f2c7f3084aa177c4c8a
crrashh1542 <1542@crrashh.com>
2026-07-16 18:04:37 +0800

    [category] 初始化category
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- packer.bat
+++ packer.bat
@@ -1,10 +1,14 @@
 @echo off
 chcp 65001
-title Windows Up-to-Date 数据打包工具 v1
+
+title Windows Up-to-Date 数据打包工具 v1.1
 
 for /f %%i in ('git rev-parse --short HEAD') do set hash=%%i
 for /f %%i in ('git rev-list --count HEAD') do set count=%%i
+for /f %%i in ('git log -1 --format^=%%cs') do set vdate=%%i
+
+echo {"hash":"%hash%","date":"%vdate%"}> %cd%\version.json
 
 set filename=data-r%count%-%hash%-%date:~5,2%%date:~8,2%%date:~11,2%
 
-"%PROGRAMFILES%\Bandizip\bz.exe" c %cd%\%filename%.zip %cd%\category\ %cd%\detail\ %cd%\latest-builds.json
+"%PROGRAMFILES%\Bandizip\bz.exe" c %cd%\%filename%.zip %cd%\category\ %cd%\detail\ %cd%\latest-builds.json %cd%\category.json %cd%\version.json
