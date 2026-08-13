48f826d3ec988f9da4eeeb69d38d559a4c4a3f3b
crrashh1542 <1542@crrashh.com>
2026-08-13 17:23:40 +0800

    [download] 添加ESD数据
    所有数据均搜集自互联网，感谢各位大佬的付出和总结
    
    借物列表：
    - 最新 25H2 ESD：https://github.com/mattieb/download-windows-esd
    - 绝大多数 ESD：https://pastebin.com/u/Ibra86
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- packer.bat
+++ packer.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001
 
-title Windows Up-to-Date 数据打包工具 v2.0
+title Windows Up-to-Date 数据打包工具 v2.1
 
 for /f %%i in ('git rev-parse --short HEAD') do set hash=%%i
 for /f %%i in ('git rev-list --count HEAD') do set count=%%i
@@ -11,7 +11,7 @@ echo {"hash":"%hash%","date":"%vdate%"}> %cd%\version.json
 
 set filename=data-r%count%-%hash%-%date:~5,2%%date:~8,2%%date:~11,2%
 
-"%PROGRAMFILES%\Bandizip\bz.exe" c %cd%\%filename%.zip %cd%\category\ %cd%\detail\ %cd%\viveid\ %cd%\index\ %cd%\version.json
+"%PROGRAMFILES%\Bandizip\bz.exe" c %cd%\%filename%.zip %cd%\category\ %cd%\detail\ %cd%\viveid\ %cd%\index\ %cd%\index\ %cd%\version.json
 
 if "%~1"=="-l" (
     goto :eof
